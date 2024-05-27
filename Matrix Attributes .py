4878b1ab89323214a49d12d3c952f6ff2c986176
ABIR MAZUMDAR <108914921+abirmazumdar03@users.noreply.github.com>
2024-05-27 21:36:58 +0530

    1st Commit
    Added two files:
    People Dataset
    A python file

---
+++ Matrix Attributes .py
@@ -0,0 +1,20 @@
+def Attributes(matrix):
+    print("Matrix Element : ")
+    for i in matrix:
+        print(i)
+    
+    print("Shape of the Array : 4X2")
+    print("Dimension of Array = 2")
+
+def Accept():
+    matrix=[];temp=[]
+
+    for i in range(4):
+        temp=[]
+        for j in range(2):
+            temp.append(int(input("Enter an Element : ")))
+        matrix.append(temp)
+    
+    Attributes(matrix)
+
+Accept()
